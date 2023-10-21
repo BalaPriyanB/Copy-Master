edc402ea3f412c163ea0abb8dc8095f69cc38857
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2023-10-21 03:26:27 -0700

    Update wserver.py

--- web/wserver.py
+++ web/wserver.py
@@ -24,7 +24,7 @@ page = """
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Torrent File Selector</title>
-    <link rel="icon" href="https://graph.org/file/1a6ad157f55bc42b548df.png" type="image/jpg">
+    <link rel="icon" href="https://graph.org/file/0dcbe15b6e69903300c67.jpg" type="image/jpg">
     <script
       src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
       integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
@@ -225,7 +225,7 @@ function s_validate() {
     <header>
       <div class="brand">
         <img
-          src="https://graph.org/file/1a6ad157f55bc42b548df.png"
+          src="https://graph.org/file/0dcbe15b6e69903300c67.jpg"
           alt="logo"
         />
         <a href="https://t.me/krn_adhikari">
@@ -233,8 +233,8 @@ function s_validate() {
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/weebzone/WZML"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/krn_adhikari"><i class="fab fa-telegram"></i></a>
+        <a href="https://www.github.com/BalaPriyanB/Copy-Master"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/TomenBots"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <div id="sticks">
@@ -421,7 +421,7 @@ code_page = """
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Torrent Code Checker</title>
-    <link rel="icon" href="https://graph.org/file/1a6ad157f55bc42b548df.png" type="image/jpg">
+    <link rel="icon" href="https://graph.org/file/0dcbe15b6e69903300c67.jpg" type="image/jpg">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
@@ -617,16 +617,16 @@ section span{
     <header>
       <div class="brand">
         <img
-          src="https://graph.org/file/1a6ad157f55bc42b548df.png"
+          src="https://graph.org/file/0dcbe15b6e69903300c67.jpg"
           alt="logo"
         />
-        <a href="https://t.me/krn_adhikari">
+        <a href="https://t.me/TomenBots">
           <h2 class="name">Bittorrent Selection</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/weebzone/WZML"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/krn_adhikari"><i class="fab fa-telegram"></i></a>
+        <a href="https://www.github.com/BalaPriyanB/Copy-Master"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/WZML_X"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -781,7 +781,70 @@ def set_priority(id_):
 
 @app.route('/')
 def homepage():
-    return "<h1>Checkout WZML-X <a href='https://www.github.com/weebzone/WZML-X'>@GitHub</a> By <a href='https://github.com/weebzone'>WZML-X Devs</a></h1>"
+    return """
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+      rel="stylesheet"
+    />
+    <style>
+        body {
+            background-color: #0D1117;
+            color: white;
+            font-family: "Ubuntu", sans-serif;
+        }
+        .header {
+            background-color: black;
+            text-align: center;
+            width: 100%;
+            padding: 1px;
+        }
+        .footer {
+            background-color: black;
+            padding: 10px;
+            text-align: center;
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+        }
+        .content {
+            padding: 20px;
+            text-align: center;
+        }
+        .button {
+            background-color: #0001f0;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+        .image {
+            border-radius: 12px;
+            max-width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <h1>TomenBots</h1>
+    </div>
+    <div class="content">
+        <img src="https://graph.org/file/0dcbe15b6e69903300c67.jpg" class="image">
+        <a href="https://telegram.me/TomenBots" style="text-decoration: none;">
+            <button class="button">Join Updates Channel Now</button>
+        </a>
+    </div>
+    <div class="footer">
+© 2022-23 WZML-X. All Rights Reserved.
+    </div>
+</body>
+</html>
+"""
 
 
 @app.errorhandler(Exception)
