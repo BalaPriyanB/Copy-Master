e530891b68f14658da4c9b1c4312764b0c8dae6b
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2023-10-22 10:09:54 -0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -36,6 +36,7 @@ class _BotCommands:
         self.RestartCommand = [f'restart{CMD_SUFFIX}', f'r{CMD_SUFFIX}', 'restartall']
         self.StatsCommand = [f'stats{CMD_SUFFIX}', f'st{CMD_SUFFIX}']
         self.HelpCommand = f'help{CMD_SUFFIX}'
+        self.BypassCommand = [f'bypass{CMD_SUFFIX}', f'bp{CMD_SUFFIX}']
         self.LogCommand = f'log{CMD_SUFFIX}'
         self.ShellCommand = f'shell{CMD_SUFFIX}'
         self.EvalCommand = f'eval{CMD_SUFFIX}'
