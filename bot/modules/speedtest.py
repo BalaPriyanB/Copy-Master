651540eaf1f08fc827e97e2ed7f0015284f25070
BalaPriyanB <117967016+BalaPriyanB@users.noreply.github.com>
2023-10-21 22:55:49 -0700

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-from speedtest import Speedtest
+from speedtest import Speedtest, ConfigRetrievalError
 from pyrogram.handlers import MessageHandler
 from pyrogram.filters import command
 
@@ -11,8 +10,12 @@ from bot.helper.ext_utils.bot_utils import get_readable_file_size, new_task
 
 @new_task
 async def speedtest(_, message):
-    speed = await sendMessage(message, "<i>Initializing Speedtest...</i>")
-    test = Speedtest()
+    speed = await sendMessage(message, "<i>Initiating Speedtest...</i>")
+    try:
+        test = Speedtest()
+    except ConfigRetrievalError:
+        await editMessage(speed, "<b>ERROR:</b> <i>Can't connect to Server at the Moment, Try Again Later !</i>")
+        return
     test.get_best_server()
     test.download()
     test.upload()
@@ -20,36 +23,36 @@ async def speedtest(_, message):
     result = test.results.dict()
     path = result['share']
     string_speed = f'''
-➲ <b><i>SPEEDTEST INFO</i></b>
-┠ <b>Upload:</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
-┠ <b>Download:</b>  <code>{get_readable_file_size(result['download'] / 8)}/s</code>
-┠ <b>Ping:</b> <code>{result['ping']} ms</code>
-┠ <b>Time:</b> <code>{result['timestamp']}</code>
-┠ <b>Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
-┖ <b>Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
+<b><i>SPEEDTEST INFO</i></b>
+⤑ <b>Upload:</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
+⤑ <b>Download:</b>  <code>{get_readable_file_size(result['download'] / 8)}/s</code>
+⤑ <b>Ping:</b> <code>{result['ping']} ms</code>
+⤑ <b>Time:</b> <code>{result['timestamp']}</code>
+⤑ <b>Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
+⤑ <b>Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
 
-➲ <b><i>SPEEDTEST SERVER</i></b>
-┠ <b>Name:</b> <code>{result['server']['name']}</code>
-┠ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
-┠ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
-┠ <b>Latency:</b> <code>{result['server']['latency']}</code>
-┠ <b>Latitude:</b> <code>{result['server']['lat']}</code>
-┖ <b>Longitude:</b> <code>{result['server']['lon']}</code>
+<b><i>SPEEDTEST SERVER</i></b>
+⤑ <b>Name:</b> <code>{result['server']['name']}</code>
+⤑ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+⤑ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
+⤑ <b>Latency:</b> <code>{result['server']['latency']}</code>
+⤑ <b>Latitude:</b> <code>{result['server']['lat']}</code>
+⤑ <b>Longitude:</b> <code>{result['server']['lon']}</code>
 
-➲ <b><i>CLIENT DETAILS</i></b>
-┠ <b>IP Address:</b> <code>{result['client']['ip']}</code>
-┠ <b>Latitude:</b> <code>{result['client']['lat']}</code>
-┠ <b>Longitude:</b> <code>{result['client']['lon']}</code>
-┠ <b>Country:</b> <code>{result['client']['country']}</code>
-┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
-┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+<b><i>CLIENT DETAILS</i></b>
+⤑ <b>IP Address:</b> <code>{result['client']['ip']}</code>
+⤑ <b>Latitude:</b> <code>{result['client']['lat']}</code>
+⤑ <b>Longitude:</b> <code>{result['client']['lon']}</code>
+⤑ <b>Country:</b> <code>{result['client']['country']}</code>
+⤑ <b>ISP:</b> <code>{result['client']['isp']}</code>
+⤑ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
 '''
     try:
         pho = await sendMessage(message, string_speed, photo=path)
         await deleteMessage(speed)
     except Exception as e:
         LOGGER.error(str(e))
-        pho = await editMessage(speed, string_speed)
+        await editMessage(speed, string_speed)
 
 bot.add_handler(MessageHandler(speedtest, filters=command(
     BotCommands.SpeedCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
